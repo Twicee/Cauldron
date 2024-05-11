b51f8397a71199dba866a2c829e7d5232ed8455b
Adrian Hernandez <hernandeza257@gmail.com>
2024-05-11 16:51:39 -0700

    Changing barrel purchasing order

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -72,27 +72,6 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     if gold <= 0:
         return plan
     
-    #Purchase a small green barrel
-    if gold >= 100 and (total_ml + 500) <= ml_capacity:
-        if gold >= 250 and (total_ml + 2500) <= ml_capacity:
-            plan.append(
-                {
-                    "sku": "MEDIUM_GREEN_BARREL",
-                    "quantity": 1,
-                }
-            )
-            gold = gold - 250
-            total_ml = total_ml + 2500
-        else:
-            plan.append(
-                {
-                    "sku": "SMALL_GREEN_BARREL",
-                    "quantity": 1,
-                }
-            )
-            gold = gold - 100
-            total_ml = total_ml + 500
-    
     #Purchase a small red barrel
     if gold >= 100 and (total_ml + 500) <= ml_capacity:
         if gold >= 250 and (total_ml + 2500) <= ml_capacity:
@@ -145,6 +124,27 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         )
         gold = gold - 750
         total_ml = total_ml + 10000
+    
+    #Purchase a small green barrel
+    if gold >= 100 and (total_ml + 500) <= ml_capacity:
+        if gold >= 250 and (total_ml + 2500) <= ml_capacity:
+            plan.append(
+                {
+                    "sku": "MEDIUM_GREEN_BARREL",
+                    "quantity": 1,
+                }
+            )
+            gold = gold - 250
+            total_ml = total_ml + 2500
+        else:
+            plan.append(
+                {
+                    "sku": "SMALL_GREEN_BARREL",
+                    "quantity": 1,
+                }
+            )
+            gold = gold - 100
+            total_ml = total_ml + 500
     
     print(plan) #delete - only for testing purposes 
     return plan
